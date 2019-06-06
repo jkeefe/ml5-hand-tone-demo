548b06dadda6691babac6f279d5fc900a8f21e5f
John Keefe <john@johnkeefe.net>
2019-06-05 23:27:16 -0500

    fix headline

--- sketch.js
+++ sketch.js
@@ -72,7 +72,7 @@ function setup() {
 
 // this is what heppens once the model is loaded
 function modelReady() {
-    select('#status').html('Model Loaded! Turn on the audio and use your right hand to change the tone. (You may need to step back a bit.)');
+    select('#status').html('Model Loaded! Turn on the audio and use your right hand to change the tone. (You may need to step back.)');
     button.show()
     osc.amp(0);
 }
